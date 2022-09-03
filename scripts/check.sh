185ba6889abfd5881028f4e1b3d8dcffb0f3d06d
Stephen Verderame <sev47@cornell.edu>
2022-09-02 22:06:00 -0400

    Working directory versions of scripts for CI

--- scripts/check.sh
+++ scripts/check.sh
@@ -3,10 +3,5 @@
 # Manually performs static analysis 
 
 pushd "$CMR_ROOT/terra" &> /dev/null
-format_files=$(find ./src -type f -name "*.cpp" -or -name "*.hpp" -or -name "*.inl")
-clang-format --dry-run --Werror $format_files
-echo "Formatting done"
-time find ./src -type f -name "*.cpp" -or -name "*.inl" \
- | xargs clang-tidy -p build/compile_commands.json --config-file=.clang-tidy \
-  --header-filter="^cmr_.*pp$"
+bash scripts/check_wd.sh
 popd &> /dev/null

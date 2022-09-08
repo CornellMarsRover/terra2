a865a28a8a7f7804cf5e126d1d17b0c69cf3d7d2
Stephen Verderame <sev47@cornell.edu>
2022-09-08 18:02:18 +0000

    Forget the '*'

--- src/cmr_fabric/test/fault_test.sh
+++ src/cmr_fabric/test/fault_test.sh
@@ -2,14 +2,14 @@
 
 ros2 launch cmr_demo demo.launch.py
 active_nodes=$(ros2 node list)
-if ! [[ $active_nodes == "/demo1" ]]; then
+if ! [[ $active_nodes == *"/demo1"* ]]; then
     echo "Test failed: demo1 node not active"
     exit 1
 fi
-if ! [[ $active_nodes == "/demo2" ]]; then
+if ! [[ $active_nodes == *"/demo2"* ]]; then
     echo "Test failed: demo2 node not active"
     exit 1
 fi
 
-ros2 topic 
+# ros2 topic 
 

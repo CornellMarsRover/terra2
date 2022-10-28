1e0e58e2103ff01d4198b366d45ff0bdf08e1904
Fynn <fdatoo7@gmail.com>
2022-10-28 02:55:05 +0000

    Arm mocking

---
+++ src/cmr_demo_py/cmr_demo_py/arm_effort_control.py
@@ -0,0 +1,36 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Float64MultiArray
+
+
+class ArmEffortControl(Node):
+    def __init__(self):
+        super().__init__("arm_effort_control")
+        self.publisher_ = self.create_publisher(
+            Float64MultiArray, "/arm_controller/commands", 10
+        )
+
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_cb)
+
+    def timer_cb(self):
+        msg = Float64MultiArray()
+        msg.data = [10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0]
+
+        self.publisher_.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = ArmEffortControl()
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()

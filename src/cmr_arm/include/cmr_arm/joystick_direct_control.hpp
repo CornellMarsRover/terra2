598e82e497afa4664cd02331093ae565600a2dbd
Daniel <dzhan27@gmail.com>
2022-10-31 01:11:23 +0000

    significant changes to arm node based on pr comments. swapped regular publisher out for lifecycle publisher, created node activation check for subscriber callback function, added buffer size and sensitivity scaler to config file, abstracted control and axis ids to constants in msg file, and explicitly activated/deactivated/cleaned pubs/subs

--- src/cmr_arm/include/cmr_arm/joystick_direct_control.hpp
+++ src/cmr_arm/include/cmr_arm/joystick_direct_control.hpp
@@ -4,23 +4,42 @@
 #include "cmr_msgs/msg/joystick_reading.hpp"
 namespace cmr
 {
+/**
+ * @brief The JoystickDirectControl node is the node called to activate arm control
+ * via direct joystick control (joystick hardware directly controls joint motor
+ * efforts). A subscriber is set up to listen for messages, and a callback function
+ * sends messages via one of many publishers to individual joint motors.
+ */
 class JoystickDirectControl : public cmr::fabric::FabricNode
 {
+  private:
+    /**
+     * Publishers and subcribers declared here. One subscriber listens for messages
+     * from the joystick hardware, and one publisher for each individual joint motor.
+     */
     std::shared_ptr<rclcpp::Subscription<cmr_msgs::msg::JoystickReading>>
         m_joystick_sub;
-    std::shared_ptr<rclcpp::Publisher<cmr_msgs::msg::ArmJointEffort>>
+    std::shared_ptr<
+        rclcpp_lifecycle::LifecyclePublisher<cmr_msgs::msg::ArmJointEffort>>
         m_base_rotate_effort_pub;
-    std::shared_ptr<rclcpp::Publisher<cmr_msgs::msg::ArmJointEffort>>
+    std::shared_ptr<
+        rclcpp_lifecycle::LifecyclePublisher<cmr_msgs::msg::ArmJointEffort>>
         m_shoulder_effort_pub;
-    std::shared_ptr<rclcpp::Publisher<cmr_msgs::msg::ArmJointEffort>>
+    std::shared_ptr<
+        rclcpp_lifecycle::LifecyclePublisher<cmr_msgs::msg::ArmJointEffort>>
         m_elbow_effort_pub;
-    std::shared_ptr<rclcpp::Publisher<cmr_msgs::msg::ArmJointEffort>>
+    std::shared_ptr<
+        rclcpp_lifecycle::LifecyclePublisher<cmr_msgs::msg::ArmJointEffort>>
         m_second_rotate_effort_pub;
-    std::shared_ptr<rclcpp::Publisher<cmr_msgs::msg::ArmJointEffort>>
+    std::shared_ptr<
+        rclcpp_lifecycle::LifecyclePublisher<cmr_msgs::msg::ArmJointEffort>>
         m_third_tilt_effort_pub;
-    std::shared_ptr<rclcpp::Publisher<cmr_msgs::msg::ArmJointEffort>>
+    std::shared_ptr<
+        rclcpp_lifecycle::LifecyclePublisher<cmr_msgs::msg::ArmJointEffort>>
         m_third_rotate_effort_pub;
-    std::shared_ptr<double> m_sensitivity;
+    double m_sensitivity;
+    double m_sens_scaler;
+    bool m_is_activated;
 
   public:
     /**
@@ -42,7 +61,13 @@ class JoystickDirectControl : public cmr::fabric::FabricNode
 
     bool cleanup() override;
 
-    bool update_arm_position(const cmr_msgs::msg::JoystickReading msg);
+    /**
+     * @brief update_arm_position is the callback function for when the subscriber
+     * receives a message from the joystick hardware. It handles logic for where to
+     * publish what message to (i.e. a certain control, axis, and magnitude number
+     * corresponds to a certain effort amount to a certain motor's axis.)
+     */
+    void update_arm_position(const cmr_msgs::msg::JoystickReading& msg);
 };
 
 }  // namespace cmr

6a4795e105338951f787529a7525e23bb1dd2ddd
Stephen Verderame <sev47@cornell.edu>
2022-11-05 03:45:37 +0000

    Add pkg dependencies

--- src/cmr_py_test/test/arm_direct_control_test.py
+++ src/cmr_py_test/test/arm_direct_control_test.py
@@ -23,6 +23,10 @@ def send_unchecked_joystick_reading(control_id: int, axis_id: int, magnitude: fl
     time.sleep(0.3)
 
 def send_joystick_reading(control_id: int, axis_id: int, magnitude: float, idx: int, pred) -> float:
+    """
+    Sends a joystick reading and checks that the joint state has changed
+    Returns the new joint position
+    """
     joy_msg = JoystickReading()
     joy_msg.axis_id = axis_id
     joy_msg.control_id = control_id
@@ -41,6 +45,7 @@ def send_joystick_reading(control_id: int, axis_id: int, magnitude: float, idx: 
     assert abs(msg.velocity[idx]) <= sys.float_info.epsilon
     return last_pos
 
+# TODO(sev47): The test fixture class doesn't work here?
 
 @cmr_node_test([
     make_launch_file("cmr_control", "demo_arm.launch.py"),
@@ -49,7 +54,9 @@ def send_joystick_reading(control_id: int, axis_id: int, magnitude: float, idx: 
 def test_basic_direct_control(namespace: str):
     assert activate_fabric_node("joystick_direct_control", namespace)
 
-    time.sleep(4)
+    time.sleep(4) # Give time for all nodes to start
+    # TODO(sev47): Add waiting support for launch files
+
     last_pos = send_joystick_reading(JoystickReading.MAIN_JOYSTICK_ID, 
                         JoystickReading.Y_AXIS_ID, 0.04, SHOULDER_IDX, 
                         lambda pos, vel: pos > 0.0 and vel >= 0.0)
@@ -64,6 +71,7 @@ def test_basic_direct_control(namespace: str):
                         JoystickReading.Z_AXIS_ID, -0.04, BASE_ROTATE_IDX,
                         lambda pos, vel: pos < last_pos and vel <= 0.0)
 
+    # Test elbow
     send_unchecked_joystick_reading(JoystickReading.THIRD_BUTTON_ID, 0, 0.0)
 
     last_pos = send_joystick_reading(JoystickReading.MAIN_JOYSTICK_ID, 
@@ -80,6 +88,12 @@ def test_basic_direct_control(namespace: str):
                         JoystickReading.Z_AXIS_ID, -0.04, SECOND_ROTATE_IDX,
                         lambda pos, vel: pos < last_pos and vel <= 0.0)
 
+    # Test ignore
+    send_joystick_reading(JoystickReading.MAIN_JOYSTICK_ID,
+                        JoystickReading.X_AXIS_ID, -0.04, SECOND_ROTATE_IDX,
+                        lambda pos, vel: pos == last_pos and vel <= 0.0)
+
+    # Test wrist
     send_unchecked_joystick_reading(JoystickReading.THIRD_BUTTON_ID, 0, 0.0)
 
     last_pos = send_joystick_reading(JoystickReading.MAIN_JOYSTICK_ID, 
@@ -96,6 +110,12 @@ def test_basic_direct_control(namespace: str):
                         JoystickReading.Z_AXIS_ID, -0.04, THIRD_ROTATE_IDX,
                         lambda pos, vel: pos < last_pos and vel <= 0.0)
 
+    # Test ignore
+    send_joystick_reading(JoystickReading.MAIN_JOYSTICK_ID,
+                        JoystickReading.X_AXIS_ID, -0.04, THIRD_ROTATE_IDX,
+                        lambda pos, vel: pos == last_pos and vel <= 0.0)
+
+    # Test loop back
     send_unchecked_joystick_reading(JoystickReading.THIRD_BUTTON_ID, 0, 0.0)
 
     last_pos = send_joystick_reading(JoystickReading.MAIN_JOYSTICK_ID, 
@@ -113,6 +133,7 @@ def test_basic_direct_control(namespace: str):
                         lambda pos, vel: pos < last_pos and vel <= 0.0)
     orig_diff = abs(pos2 - last_pos)
 
+    # Test end effector
 
     last_pos = send_joystick_reading(JoystickReading.SECOND_JOYSTICK_ID, 
                         JoystickReading.Y_AXIS_ID, 0.04, END_EFFECTOR_IDX, 
@@ -121,6 +142,7 @@ def test_basic_direct_control(namespace: str):
                         JoystickReading.Y_AXIS_ID, -0.04, END_EFFECTOR_IDX,
                         lambda pos, vel: pos < last_pos and vel <= 0.0)
 
+    # Test sensitivity
     send_unchecked_joystick_reading(JoystickReading.SENS_SLIDER_ID, 0, 200.0)
 
     last_pos = send_joystick_reading(JoystickReading.MAIN_JOYSTICK_ID, 
@@ -132,6 +154,7 @@ def test_basic_direct_control(namespace: str):
 
     assert abs(pos2 - last_pos) > orig_diff
 
+    # Test reset sensitivity
     send_unchecked_joystick_reading(JoystickReading.SENS_SLIDER_ID, 0, 100.0)
 
     last_pos = send_joystick_reading(JoystickReading.MAIN_JOYSTICK_ID, 
@@ -144,11 +167,17 @@ def test_basic_direct_control(namespace: str):
     assert abs(abs(pos2 - last_pos) - orig_diff) < 0.1
 
 
+    # Test multi messages
     for _ in range(5):
         send_unchecked_joystick_reading(JoystickReading.MAIN_JOYSTICK_ID, JoystickReading.Y_AXIS_ID, 0.05)
 
     last_pos = send_joystick_reading(JoystickReading.MAIN_JOYSTICK_ID,
                                     JoystickReading.Y_AXIS_ID, 0.04, SHOULDER_IDX,
                                     lambda pos, vel: pos > max(last_pos * 3, 1.) and vel >= 0.0)
+
+    # Test ignore
+    send_joystick_reading(JoystickReading.MAIN_JOYSTICK_ID,
+                          JoystickReading.X_AXIS_ID, 0.04, SHOULDER_IDX,
+                          lambda pos, vel: pos == last_pos and vel >= 0.0)
     
     

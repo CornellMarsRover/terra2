0b2bf57b619561791a8922683641e2eddc73d8d0
Stephen Verderame <sev47@cornell.edu>
2022-11-15 00:41:51 +0000

    Try pushd

--- scripts/code_coverage.sh
+++ scripts/code_coverage.sh
@@ -4,6 +4,8 @@
 # Builds html output in build/cov-output directory
 # Assumes the working directory is the terra root directory
 
+pushd "$CMR_ROOT/terra" &> /dev/null
+
 lcov --directory . --base-directory . "$@"  --capture -o build/cov.info > /dev/null
 lcov -r build/cov.info "/opt/*" -o build/cov.info > /dev/null
 lcov -r build/cov.info "*/cmr_msgs/*" -o build/cov.info > /dev/null
@@ -13,3 +15,5 @@ lcov -r build/cov.info "*/external/*" -o build/cov.info > /dev/null
 # Remove c++ standard library
 lcov -r build/cov.info "/usr/*" -o build/cov.info > /dev/null
 genhtml build/cov.info -o build/cov-output
+
+popd &>/dev/null

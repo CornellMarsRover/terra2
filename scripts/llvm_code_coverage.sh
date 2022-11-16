b4a244dc6c39fff5a2822c513733a740b8577ef4
Stephen Verderame <sev47@cornell.edu>
2022-11-16 04:06:26 +0000

    Hardcode path

--- scripts/llvm_code_coverage.sh
+++ scripts/llvm_code_coverage.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-bash scripts/code_coverage.sh --gcov-tool scripts/llvm-cov-wrapper.sh
+# This is hacky to hard code the path to the wrapper for the CI
+bash scripts/code_coverage.sh --gcov-tool /__w/terra/terra/scripts/llvm-cov-wrapper.sh

d9a715a3c189168cc04626373be009260c621e98
Stephen Verderame <sev47@cornell.edu>
2023-01-13 19:06:00 +0000

    Nav2 bringup

--- src/cmr_description/cmr_drives_description/launch/view_drives.launch.py
+++ src/cmr_description/cmr_drives_description/launch/view_drives.launch.py
@@ -1,7 +1,7 @@
 
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
 from launch.substitutions import (
     Command,
     FindExecutable,
@@ -14,19 +14,50 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 import os
 from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 
 
 def generate_launch_description():
     pkg_share = FindPackageShare(package='cmr_drives_description').find('cmr_drives_description')
+    bringup_dir = get_package_share_directory('nav2_bringup')
+    slam_dir = get_package_share_directory('slam_toolbox')
     default_rviz_config_path = os.path.join(pkg_share, 'config/drives_view.rviz')
-    world_path= os.path.join(pkg_share, 'world/my_world.sdf')
+    default_world_path= os.path.join(pkg_share, 'world/my_world.sdf')
+    slam_launch_file = os.path.join(slam_dir, 'launch', 'online_async_launch.py')
     # Initialize arguments
     description_package = LaunchConfiguration("description_package")
     description_file = LaunchConfiguration("description_file")
     sim_time = LaunchConfiguration("use_sim_time")
     rviz_config = LaunchConfiguration("rvizconfig")
+    params_file = LaunchConfiguration("params_file")
+    use_respawn = LaunchConfiguration("use_respawn")
+    world = LaunchConfiguration("world")
+    slam_params_file = LaunchConfiguration("slam_params_file")
+
+    declare_desc_pkg_cmd = DeclareLaunchArgument(
+            "description_package",
+            default_value="cmr_drives_description",
+    )
+    declare_desc_file_cmd = DeclareLaunchArgument(
+            "description_file",
+            default_value="drives.urdf.xacro",
+    )
+    declare_rvizconfig_cmd = DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+                                            description='Absolute path to rviz config file')
+    declare_sim_time_cmd = DeclareLaunchArgument(name='use_sim_time', default_value='True',
+                                            description='Flag to enable use_sim_time')
+    declare_params_file_cmd = DeclareLaunchArgument(name='params_file', 
+                                            default_value=os.path.join(pkg_share, 'config', 'nav_params.yaml'),
+                                            description='Full path to the ROS2 parameters file to use for all launched nodes')
+    declare_use_respawn_cmd = DeclareLaunchArgument(name='use_respawn', default_value='True',
+                                            description='Flag to enable respawn of nav2 nodes if they die')
+    declare_world_cmd = DeclareLaunchArgument(name='world', default_value=default_world_path,
+                                            description='Full path to world file to load')
+    declare_slam_params_cmd = DeclareLaunchArgument(name='slam_params_file',
+                                            default_value=os.path.join(pkg_share, 'config', 'mapper_params_online_async.yaml'),
+                                            description='Full path to the ROS2 parameters file to use for all launched nodes')
 
     # Generate URDF file via Xacro
     robot_description_content = Command(
@@ -39,7 +70,7 @@ def generate_launch_description():
         ]
     )
     robot_description = {"robot_description": robot_description_content, 
-                         "use_sim_time": True}
+                         "use_sim_time": sim_time}
 
     
     robot_state_publisher_node = Node(
@@ -51,8 +82,7 @@ def generate_launch_description():
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
-        parameters=[{'use_sim_time': True}]
-        # condition=UnlessCondition(LaunchConfiguration('gui'))
+        parameters=[{'use_sim_time': sim_time}]
     )
     rviz_node = Node(
         package='rviz2',
@@ -60,7 +90,7 @@ def generate_launch_description():
         name='rviz2',
         output='screen',
         arguments=['-d', rviz_config],
-        parameters=[{'use_sim_time': True}]
+        parameters=[{'use_sim_time': sim_time}]
     )
     spawn_entity = Node(
         package='gazebo_ros', 
@@ -68,35 +98,63 @@ def generate_launch_description():
         arguments=['-entity', 'cmr_drives', '-topic', 'robot_description'],
         output='screen'
     )
-    start_sync_slam_toolbox_node = Node(
-        parameters=[
-          os.path.join(get_package_share_directory("cmr_test"), 'config/mapper_params_online_sync.yaml'),
-          {'use_sim_time': sim_time}
-        ],
-        package='slam_toolbox',
-        executable='sync_slam_toolbox_node',
-        name='slam_toolbox',
-        output='screen')
+    # start_sync_slam_toolbox_node = Node(
+    #     parameters=[
+    #       os.path.join(pkg_share, 'config/mapper_params_online_sync.yaml'),
+    #       {'use_sim_time': sim_time}
+    #     ],
+    #     package='slam_toolbox',
+    #     executable='sync_slam_toolbox_node',
+    #     name='slam_toolbox',
+    #     output='screen')
+
+    start_slam_toolbox = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(slam_launch_file),
+        launch_arguments={'use_sim_time': sim_time,
+                          'slam_params_file': slam_params_file}.items(),
+    )
+    
+    start_gazebo = ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 
+                'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world], output='screen')
+
+    # nav_bringup = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')),
+    #     launch_arguments={#'namespace': namespace,
+    #                       #'use_namespace': use_namespace,
+    #                       'map': "",
+    #                       'use_sim_time': sim_time,
+    #                       'params_file': params_file,
+    #                       'slam': 'True',
+    #                       #'autostart': autostart,
+    #                       'use_composition': 'True',
+    #                       'use_respawn': use_respawn
+    #                       }.items()
+    # )
+    nav_bringup = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'navigation_launch.py')),
+        launch_arguments={'use_sim_time': sim_time,
+                          'params_file': params_file,
+                          'use_respawn': use_respawn,
+                          'use_composition': 'True',
+                         }.items()
+    )
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            "description_package",
-            default_value="cmr_drives_description",
-        ),
-        DeclareLaunchArgument(
-            "description_file",
-            default_value="drives.urdf.xacro",
-        ),
-        DeclareLaunchArgument(name='gui', default_value='True',
-                                            description='Flag to enable joint_state_publisher_gui'),
-        DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                            description='Absolute path to rviz config file'),
-        DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                                            description='Flag to enable use_sim_time'),
-        ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
+        declare_desc_pkg_cmd,
+        declare_desc_file_cmd,
+        declare_rvizconfig_cmd,
+        declare_sim_time_cmd,
+        declare_params_file_cmd,
+        declare_use_respawn_cmd,
+        declare_world_cmd,
+        declare_slam_params_cmd,
+        start_gazebo,
         joint_state_publisher_node,
         robot_state_publisher_node,
         spawn_entity,
-        start_sync_slam_toolbox_node,
+        start_slam_toolbox,
         rviz_node,
+        nav_bringup
     ])

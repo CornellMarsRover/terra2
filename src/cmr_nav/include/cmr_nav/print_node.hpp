792562c9ee662aeaef22d81d70d2bca5cf3c9d69
Avocado <cmravocado@gmail.com>
2023-01-16 17:40:25 +0000

    Switched to using ActionSyncNode

--- src/cmr_nav/include/cmr_nav/print_node.hpp
+++ src/cmr_nav/include/cmr_nav/print_node.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 
+#include "behaviortree_cpp_v3/action_node.h"
 #include "nav2_behavior_tree/bt_action_node.hpp"
 #include "nav2_msgs/action/wait.hpp"
 
@@ -11,37 +12,42 @@ namespace cmr
 /**
  * @brief A cmr::PrintAction class that wraps nav2_msgs::action::Wait
  */
-class PrintAction : public nav2_behavior_tree::BtActionNode<nav2_msgs::action::Wait>
+class PrintAction : public BT::SyncActionNode
 {
-public:
-  /**
-   * @brief A constructor for cmr::PrintAction
-   * @param xml_tag_name Name for the XML tag for this node
-   * @param action_name Action name this node creates a client for
-   * @param conf BT node configuration
-   */
-  PrintAction(
-    const std::string & xml_tag_name,
-    const std::string & action_name,
-    const BT::NodeConfiguration & conf);
+  public:
+    /**
+     * @brief A constructor for cmr::PrintAction
+     * @param xml_tag_name Name for the XML tag for this node
+     * @param action_name Action name this node creates a client for
+     * @param conf BT node configuration
+     */
+    PrintAction(const std::string& xml_tag_name, const std::string& action_name,
+                const BT::NodeConfiguration& conf);
 
-  /**
-   * @brief Function to perform some user-defined operation on tick
-   */
-  void on_tick() override;
+    /**
+     * @brief Function to perform some user-defined operation on tick
+     */
+    void on_tick();
 
-  /**
-   * @brief Creates list of BT ports
-   * @return BT::PortsList Containing basic ports along with node-specific ports
-   */
-  static BT::PortsList providedPorts()
-  {
-    return providedBasicPorts(
-      {
-        BT::InputPort<std::string>("message", "", "message to print")
-      });
-  }
+    /**
+     * @brief Creates list of BT ports
+     * @return BT::PortsList Containing basic ports along with node-specific ports
+     */
+    // NOLINTNEXTLINE(readability-identifier-naming)
+    static BT::PortsList providedPorts()
+    {
+        return {BT::InputPort<std::string>("message", "", "message to print")};
+    }
+
+    virtual BT::NodeStatus on_success() { return BT::NodeStatus::SUCCESS; }
+    virtual BT::NodeStatus on_aborted() { return BT::NodeStatus::FAILURE; }
+    virtual BT::NodeStatus on_cancelled() { return BT::NodeStatus::SUCCESS; }
+
+    BT::NodeStatus tick() override
+    {
+        on_tick();
+        return BT::NodeStatus::SUCCESS;
+    }
 };
 
 }  // namespace cmr
-
--- src/cmr_nav/src/print_node.cpp
+++ src/cmr_nav/src/print_node.cpp
@@ -1,25 +1,26 @@
-#include <string>
-#include <memory>
-
 #include "cmr_nav/print_node.hpp"
+
+#include <memory>
+#include <string>
 
 namespace cmr
 {
 std::string g_message;
-PrintAction::PrintAction(
-  const std::string & xml_tag_name,
-  const std::string & action_name,
-  const BT::NodeConfiguration & conf)
-: BtActionNode<nav2_msgs::action::Wait>(xml_tag_name, action_name, conf)
+// NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+PrintAction::PrintAction(const std::string& xml_tag_name,
+                         const std::string&,
+                         const BT::NodeConfiguration& conf)
+    : BT::SyncActionNode(xml_tag_name, conf)
 {
-  std::string message;
-  getInput("message", message);
-  g_message = message;
+    std::string message;
+    getInput("message", message);
+    g_message = message;
 }
 
 void PrintAction::on_tick()
 {
-    RCLCPP_INFO(node_->get_logger(), "Message: %s. \n", g_message.c_str());
+    RCLCPP_INFO(rclcpp::get_logger("Print Logger"), "Message: %s. \n",
+                g_message.c_str());
 }
 
 }  // namespace cmr
@@ -27,11 +28,10 @@ void PrintAction::on_tick()
 #include "behaviortree_cpp_v3/bt_factory.h"
 BT_REGISTER_NODES(factory)
 {
-  BT::NodeBuilder builder =
-    [](const std::string & name, const BT::NodeConfiguration & config)
-    {
-      return std::make_unique<cmr::PrintAction>(name, "print", config);
+    BT::NodeBuilder builder = [](const std::string& name,
+                                 const BT::NodeConfiguration& config) {
+        return std::make_unique<cmr::PrintAction>(name, "print", config);
     };
 
-  factory.registerBuilder<cmr::PrintAction>("Print", builder);
+    factory.registerBuilder<cmr::PrintAction>("Print", builder);
 }

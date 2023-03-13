41e0f5eb1083830175805910b2aa90ca93027143
Avocado <cmravocado@gmail.com>
2023-03-13 20:31:08 +0000

    saving astrotech initial code

---
+++ src/cmr_astrotech/include/cmr_astrotech/site_analyze.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include "cmr_fabric/fabric_node.hpp"
+#include "cmr_msgs/msg/motor_write_batch.hpp"
+#include "cmr_msgs/srv/site_analyze.hpp"
+
+namespace cmr
+{
+
+/**
+ * `SiteAnalyze`
+ *
+ * TODO
+ */
+class SiteAnalyze : public cmr::fabric::FabricNode
+{
+  public:
+    /**
+     * Constructs a `SiteAnalyze`, optionally passing in config parameters for
+     * testing.
+     *
+     * @param config the configuration struct for starting the node or an empty
+     * optional to start the node from a launch file or via ROS
+     */
+    explicit SiteAnalyze(
+        const std::optional<cmr::fabric::FabricNodeConfig>& config = std::nullopt);
+
+  private:
+    std::shared_ptr<rclcpp::Node> m_node;
+    rclcpp::Publisher<cmr_msgs::msg::MotorWriteBatch>::SharedPtr
+        m_motor_state_publisher;
+    rclcpp::Service<cmr_msgs::srv::SiteAnalyze>::SharedPtr m_service;
+
+    void handle_request(
+        const std::shared_ptr<cmr_msgs::srv::SiteAnalyze::Request> request,
+        std::shared_ptr<cmr_msgs::srv::SiteAnalyze::Response> response);
+
+    void fill(std::vector<int> sites);
+
+    void analyze();
+
+    bool configure(const std::shared_ptr<toml::Table>& table) override;
+
+    bool activate() override;
+
+    bool deactivate() override;
+
+    bool cleanup() override;
+};
+
+}  // namespace cmr
---
+++ src/cmr_astrotech/src/site_analyze.cpp
@@ -0,0 +1,112 @@
+#include "cmr_astrotech/site_analyze.hpp"
+
+namespace cmr
+{
+
+SiteAnalyze::SiteAnalyze(const std::optional<cmr::fabric::FabricNodeConfig>& config)
+    : cmr::fabric::FabricNode::FabricNode(config)
+{
+    m_node = rclcpp::Node::make_shared("site_analyze_server");
+    m_service = m_node->create_service<cmr_msgs::srv::SiteAnalyze>("site_analyze",
+                                                                   &handle_request);
+    m_motor_state_publisher =
+        m_node->create_publisher<cmr_msgs::msg::MotorWriteBatch>(
+            "/motor", rclcpp::SystemDefaultsQoS());
+}
+
+void SiteAnalyze::handle_request(
+    const std::shared_ptr<cmr_msgs::srv::SiteAnalyze::Request> request,
+    std::shared_ptr<cmr_msgs::srv::SiteAnalyze::Response> response)
+{
+    response->success = true;
+    switch (request->site_num) {
+        case 1:
+            for (int i = 0; i < 4; i++) {
+                fill({1});
+                analyze();
+            }
+            break;
+        case 2:
+            fill({1, 2});
+            analyze();
+            for (int i = 0; i < 3; i++) {
+                fill({2});
+                analyze();
+            }
+            break;
+        case 3:
+            break;
+        case 4:
+            fill({2, 3});
+            analyze();
+            for (int i = 0; i < 3; i++) {
+                fill({3});
+                analyze();
+            }
+            fill({3, 4});
+            analyze();
+
+            for (int i = 0; i < 4; i++) {
+                fill({4});
+                analyze();
+            }
+
+            for (int i = 0; i < 3; i++) {
+                analyze();
+            }
+            break;
+        default:
+            response->success = false;
+            break;
+    }
+}
+
+void SiteAnalyze::analyze() {
+    fill({5});
+}
+
+void SiteAnalyze::fill(std::vector<int> sites)
+{
+    cmr_msgs::msg::MotorWriteBatch msg{};
+    msg.size = static_cast<int32_t>(sites.size());
+
+    for (int i : sites) {
+        msg.motor_ids.push_back(i + 217);
+        msg.control_modes.push_back(2);
+        msg.values.push_back(20);
+    }
+    m_motor_state_publisher->publish(msg);
+}
+
+bool SiteAnalyze::configure(const std::shared_ptr<toml::Table>&)
+{
+    // read node config; setup subscriptions, clients, services, etc.; and
+    // most of the node setup logic here
+
+    // Ex. const auto node_settings = table->getTable("node");
+    return true;
+}
+
+bool SiteAnalyze::activate()
+{
+    // do any last-minute things before activation here
+    // it should be quick
+
+    return true;
+}
+
+bool SiteAnalyze::deactivate()
+{
+    // undo the effects of activate here
+
+    return true;
+}
+
+bool SiteAnalyze::cleanup()
+{
+    // undo the effects of configure here
+
+    return true;
+}
+
+}  // namespace cmr

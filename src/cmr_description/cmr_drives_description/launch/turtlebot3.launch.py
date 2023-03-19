1aecc99c99b97c9084c947636631882062d31ba7
Stephen Verderame <sev47@cornell.edu>
2023-03-19 14:17:37 +0000

    Change defaults

--- src/cmr_description/cmr_drives_description/launch/turtlebot3.launch.py
+++ src/cmr_description/cmr_drives_description/launch/turtlebot3.launch.py
@@ -81,7 +81,7 @@ def generate_launch_description():
 
     declare_slam_cmd = DeclareLaunchArgument(
         'slam',
-        default_value='False',
+        default_value='True',
         description='Whether run a SLAM')
 
     declare_map_yaml_cmd = DeclareLaunchArgument(
@@ -98,7 +98,7 @@ def generate_launch_description():
     declare_params_file_cmd = DeclareLaunchArgument(
         'params_file',
         #default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
-        default_value=os.path.join(drives_dir, 'config', 'nav2_params_turtle.yaml'),
+        default_value=os.path.join(drives_dir, 'config', 'nav_params_turtle.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
 
     declare_autostart_cmd = DeclareLaunchArgument(
@@ -136,7 +136,7 @@ def generate_launch_description():
 
     declare_simulator_cmd = DeclareLaunchArgument(
         'headless',
-        default_value='True',
+        default_value='False',
         description='Whether to execute gzclient)')
 
     declare_world_cmd = DeclareLaunchArgument(

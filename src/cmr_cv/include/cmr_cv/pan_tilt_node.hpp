ada8aa45fdd93ca392ab2352d915a247439c1390
Avocado <cmravocado@gmail.com>
2023-05-31 03:46:06 +0000

    Create pan-tilt camera node

---
+++ src/cmr_cv/include/cmr_cv/pan_tilt_node.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "cmr_fabric/fabric_node.hpp"
+
+namespace cmr
+{
+
+/**
+ * `PanTiltNode`
+ *
+ * TODO
+ */
+class PanTiltNode : public cmr::fabric::FabricNode
+{
+  public:
+    /**
+     * Constructs a `PanTiltNode`, optionally passing in config parameters for
+     * testing.
+     *
+     * @param config the configuration struct for starting the node or an empty optional
+     * to start the node from a launch file or via ROS
+     */
+    explicit PanTiltNode(
+        const std::optional<cmr::fabric::FabricNodeConfig>& config = std::nullopt);
+
+  private:
+    bool configure(const std::shared_ptr<toml::Table>& table) override;
+
+    bool activate() override;
+
+    bool deactivate() override;
+
+    bool cleanup() override;
+};
+
+} // namespace cmr

7f9ad74bd48ac247e2baf4f8abb71afb9b8d61da
cmravocado <cmravocado@gmail.com>
2024-01-17 15:40:32 -0500

    Edited to fit angular commands

--- src/cmr_rovernet/cmr_rovernet/drivesnet.py
+++ src/cmr_rovernet/cmr_rovernet/drivesnet.py
@@ -16,6 +16,7 @@ class CmdVelSubscriber(Node):
             10)
         self.subscription  # prevent unused variable warning
         self.current_speed = 0
+        self.current_speed_angular = 0
         self.last_time = time.time()
         self.port = "/dev/ttyTHS0"
         self.baud_rate = 115200
@@ -46,10 +47,36 @@ class CmdVelSubscriber(Node):
             if self.current_speed < target_speed:
                 self.current_speed = target_speed
 
+    def gradually_increase_speed_angular(self, target_speed):
+        current_time = time.time()
+        time_difference = current_time - self.last_time
+        self.last_time = current_time
+
+        # Base rate of speed change
+        base_rate = 24 * time_difference
+
+        # Modify the rate based on how close the target speed is to neutral (0)
+        # The rate of change is faster when the joystick is near neutral
+        dynamic_rate = base_rate * (1 - abs(target_speed) / 35)  # Assuming 35 is the max speed
+
+        # Ensure dynamic_rate does not drop below a certain threshold
+        dynamic_rate = max(dynamic_rate, base_rate * 0.5)
+
+        if self.current_speed_angular < target_speed:
+            self.current_speed_angular += dynamic_rate
+            if self.current_speed_angular > target_speed:
+                self.current_speed_angular = target_speed
+        elif self.current_speed_angular > target_speed:
+            self.current_speed_angular -= dynamic_rate
+            if self.current_speed_angular < target_speed:
+                self.current_speed_angular = target_speed
+
 
     def listener_callback(self, msg):
         target_speed = scale_value(msg.twist.linear.x, -2.5, 2.5, -53, 53)
+        target_speed_angular = scale_value(msg.twist.angular.y, -2.5, 2.5, -53, 53)
         self.gradually_increase_speed_linear(target_speed)
+        self.gradually_increase_speed_angular(target_speed_angular)
         # if msg.twist.angular.z == 2.5: 
         #    output = byte_command_converter("drives", "back_right", None, None, None, None, None, self.get_logger())
         if msg.twist.linear.x != 0:
@@ -63,10 +90,10 @@ class CmdVelSubscriber(Node):
             send_number(self.serial_port, back_left)
         
         if msg.twist.angular.z != 0:
-            back_right = byte_command_converter("drives", "back_right", None, -self.current_speed, 1.5, None, 5.0, self.get_logger())
-            front_right = byte_command_converter("drives", "front_right", None, -self.current_speed, 1.5, None, 5.0, self.get_logger())
-            front_left = byte_command_converter("drives", "front_left", None, self.current_speed, 1.5, None, 5.0, self.get_logger())
-            back_left = byte_command_converter("drives", "back_left", None, self.current_speed, 1.5, None, 5.0, self.get_logger())
+            back_right = byte_command_converter("drives", "back_right", None, -self.current_speed_angular, 1.5, None, 5.0, self.get_logger())
+            front_right = byte_command_converter("drives", "front_right", None, -self.current_speed_angular, 1.5, None, 5.0, self.get_logger())
+            front_left = byte_command_converter("drives", "front_left", None, self.current_speed_angular, 1.5, None, 5.0, self.get_logger())
+            back_left = byte_command_converter("drives", "back_left", None, self.current_speed_angular, 1.5, None, 5.0, self.get_logger())
             send_number(self.serial_port, back_right)
             send_number(self.serial_port, front_right)
             send_number(self.serial_port, front_left)

ec025a15123ea98ebbf374771fa3a40dd2ff9ec0
Ishaan Adhikary <IshaanAdhikary@outlook.com>
2026-02-23 18:50:01 -0500

    Created ROS package for autonomous typing

---
+++ src/autonomous_typing_package/launch/node.launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='autonomous_typing_package',
+            executable='typing_mission_ros',
+            name='autonomous_typing_publisher',
+            output='screen', #This option directs logs to the console
+        ),
+        # Add more nodes if needed
+    ])

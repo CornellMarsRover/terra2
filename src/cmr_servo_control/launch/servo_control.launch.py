428c79a7af792336925004c7d542b5d5dcfcc3fa
Aaron Babu <ab2458@cornell.edu>
2026-05-28 10:36:30 -0400

    can port by id for servo control

--- src/cmr_servo_control/launch/servo_control.launch.py
+++ src/cmr_servo_control/launch/servo_control.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
             executable='arm_servo_control_node',
             output='screen',
             parameters=[
-                {'can_port': '/dev/ttyACM1'},
+                {'can_port': '/dev/serial/by-id/usb-mjbots_fdcanusb_062C45D7-if00'},
                 {'baud': 115200},
                 {'servo_can_id': 25},
                 {'command_topic': '/arm_controller/cmd_buttons'},

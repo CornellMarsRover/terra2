6767009eb7244f715bce362b4f5cac9a23ac101f
agupt0318 <anantg001@gmail.com>
2026-09-06 17:34:27 -0400

    Limit static checks to changed C++

--- scripts/check_wd.sh
+++ scripts/check_wd.sh
@@ -3,24 +3,25 @@
 # Manually perform static analysis
 # assumes we are in terra2 directory
 
-format_files=$(find ./src -type f \( -name "*.cpp" -or -name "*.hpp" -or -name "*.inl" \) -and -not -path "*/external/*")
-clang-format --dry-run --Werror $format_files
+if [[ "$@" = "" ]]; then
+    changed_files=$(git diff --diff-filter=ACMRT --name-only HEAD~1 | grep -E '\.(hpp|cpp|inl)$' | grep -v -e ".*/external/.*")
+else
+    changed_files=$(echo "$@" | tr ' ' '\n' | grep -e ".hpp$" -e ".cpp$" -e ".inl$" | grep -v -e ".*/external/.*")
+fi
+if [[ "$changed_files" = "" ]]; then
+    echo "No changed C++ files to check"
+    exit 0
+else
+    echo "Checking files: $changed_files"
+fi
+
+clang-format --dry-run --Werror $changed_files
 if [ $? -ne 0 ]; then
     echo "clang-format contains errors"
     exit 1
 fi
 echo "Formatting done"
-if [[ "$@" = "" ]]; then
-    changed_files=$(git diff --name-only HEAD~1 | grep -e ".*cpp" -e ".*inl" | grep -v -e ".*/external/.*")
-else
-    changed_files=$(echo "$@" | tr ' ' '\n' | grep -e ".hpp$" -e ".cpp$" -e ".inl$" | grep -v -e ".*/external/.*")
-fi
-if [[ "$changed_files" = "" ]]; then
-    echo "No files to check"
-    exit 0
-else
-    echo "Checking files: $changed_files"
-fi
+
 time clang-tidy -p build/compile_commands.json --config-file=.clang-tidy \
   --header-filter="^cmr_.*pp$" $changed_files
 if [ $? -ne 0 ]; then

09fa89ccb685b7311ed939a9e98cde9c75b1f827
agupt0318 <anantg001@gmail.com>
2026-09-07 13:07:29 -0400

    Cover autonomy target contracts in CI

--- scripts/test_autonomy.sh
+++ scripts/test_autonomy.sh
@@ -6,7 +6,7 @@ cd "$ROOT"
 export PYTHONPATH="$ROOT/src/autonomous_navigation${PYTHONPATH:+:$PYTHONPATH}"
 
 python3 -m coverage erase
-PURE_MODULES="autonomous_navigation.drive_command,autonomous_navigation.planner_core,autonomous_navigation.costmap_core,autonomous_navigation.state_machine_core"
+PURE_MODULES="autonomous_navigation.drive_command,autonomous_navigation.planner_core,autonomous_navigation.costmap_core,autonomous_navigation.state_machine_core,autonomous_navigation.target_contract"
 python3 -m coverage run --branch --source="$PURE_MODULES" \
   -m pytest -q -p no:cacheprovider src/autonomous_navigation/test
 python3 -m coverage report --show-missing --fail-under=100
